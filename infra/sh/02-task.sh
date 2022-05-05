07c165157b99c7ed22c3ec948a8be3714b6014b4
sankaku <sankaku.git@gmail.com>
2022-05-05 23:21:05 +0900

    read role ARNs for Fargate from GitHub Actions, not from a file

--- infra/sh/02-task.sh
+++ infra/sh/02-task.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 cd `dirname $0` || exit 1
-. ./constants.txt
 
 PROJECT_NAME=SampleGAEcsRollback
-
 IMAGE_VERSION=$1
+
+# Environment variables below will be given by GitHub Actions.
+#
+# MY_EXECUTION_ROLE_ARN
+# MY_TASK_ROLE_ARN
 
 aws cloudformation deploy --template-file "../cfn/task.yaml" \
                           --stack-name "$PROJECT_NAME-Task" \
